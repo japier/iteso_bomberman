e19efe7ce0ae882de897706ea67654b6aed30974
Javier Castro <jcastro@ooyala.com>
2014-04-28 08:28:19 -0500

    Imagenes

--- bomberman.c
+++ bomberman.c
@@ -2,6 +2,7 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
+#include "zelda.h"
 typedef struct Personaje{
   int x;
   int y;
@@ -10,6 +11,8 @@ typedef struct Personaje{
 }PERSONAJE;
 ALLEGRO_DISPLAY * display = NULL;
 ALLEGRO_EVENT_QUEUE * event_queue = NULL;
+ALLEGRO_BITMAP * imagen = NULL;
+ALLEGRO_PATH * path = NULL;
 int tablero[20][20];
 
 PERSONAJE personaje;
@@ -17,9 +20,15 @@ int init(){
   int i, j;
 
   if(!al_init())printf("No se pudo");
+  if(!al_init_image_addon())printf("No se pudo abrir el gestor de imagenes\n");
   display = al_create_display(400, 400);
   event_queue = al_create_event_queue();
 
+  path = al_get_standard_path(ALLEGRO_RESOURCES_PATH);
+  al_change_directory(al_path_cstr(path, '/'));
+  al_destroy_path(path);
+
+  imagen = al_load_bitmap("img/zelda.gif");
   al_register_event_source(event_queue, al_get_display_event_source(display));
   for (i = 0; i < 20; ++i){
     for (j = 0; j < 20; ++j)
@@ -44,6 +53,8 @@ void logica(int x){
 }
 void dibujo(){
   al_clear_to_color(al_map_rgb(0, 0 ,255));
+  al_draw_bitmap_region(imagen, sprites[0][0], sprites[0][1], sprites[0][2], sprites[0][3], 20, 20, 0);
+
   al_flip_display();
   al_rest(1);
 }
